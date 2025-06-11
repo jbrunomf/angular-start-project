b95dc910c9ddd5aa9ade05fa246edf654c52a372
João Bruno <jbrunomf@outlook.com>
2025-06-10 21:45:52 -0300

    Refactor components: replace NgOptimizedImage with standard img tags, migrate to signals in user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [
-
-  ],
+  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+  selectedUser = signal(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
-  onSelectUser(){
-    console.log('Clicked!');
+  onSelectUser() {
+    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
   }
 }
